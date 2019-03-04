609932e147daac0ff08b1572fb05b3f7aaea94c3
asde8051 <47123953+asde8051@users.noreply.github.com>
2019-03-04 17:33:12 +0000

    Add files via upload

--- unpack.sh
+++ unpack.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-cd ~/work/week6/cwl-data/data/structured
+names='structured-2018-01-14-neworleans structured-2018-04-22-seattle structured-2018-03-11-atlanta structured-2018-06-17-anaheim structured-2018-04-01-birmingham structured-2018-07-29-proleague2 structured-2018-04-08-proleague1 structured-2018-08-19-champs structured-2018-01-14-neworleans structured-2018-04-19-relegation '
 
-for value in $1.tar.gz
+for name in $names*
 do
-tar  xvf  $value
-mv ~/work/week6/cwl-data/data/structured/$value  ~/work/week7/$value
+gunzip $name*
+tar  xvf  $name*
+mv $name  ~/work/week7/$name
 done
